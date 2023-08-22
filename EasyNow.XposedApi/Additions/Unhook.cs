99333970cc78a270d77fb7aeb711f30229e7bca0
sxi3265 <sxi3265@gmail.com>
2023-08-22 10:55:28 +0800

    建库

---
+++ EasyNow.XposedApi/Additions/Unhook.cs
@@ -0,0 +1,29 @@
+using DE.Robv.Android.Xposed.Callbacks;
+
+namespace DE.Robv.Android.Xposed.Callbacks
+{
+    public abstract partial class XC_LayoutInflated
+    {
+        public partial class Unhook
+        {
+            void IXUnhook.Unhook()
+            {
+                InvokeUnhook();
+            }
+        }
+    }
+}
+
+namespace DE.Robv.Android.Xposed
+{
+    public abstract partial class XC_MethodHook
+    {
+        public partial class Unhook
+        {
+            void IXUnhook.Unhook()
+            {
+                InvokeUnhook();
+            }
+        }
+    }
+}
